614f99a9296275479e95b6abc3063c5c78b2f436
whst <xukiro@outlook.com>
2022-03-24 23:23:49 +0800

    Fixing some typos

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Start leaning ⏱️
+            Start learning ⏱️
           </Link>
         </div>
       </div>

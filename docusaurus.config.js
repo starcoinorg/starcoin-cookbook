bcc63111787979fa7386e894f5801e30a271b865
jolestar <jolestar@gmail.com>
2022-06-06 23:27:39 +0800

    change logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,7 +62,7 @@ const config = {
         title: 'Starcoin Cookbook',
         logo: {
           alt: 'Starcoin Cookbook',
-          src: '/img/logo.svg',
+          src: '/img/logo.png',
         },
         items: [
           {
